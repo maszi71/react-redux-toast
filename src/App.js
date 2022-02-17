7b0b982fda5772ce266aa5e3e0aafce596d818e8
masoudvrz <masoud.varzi@yahoo.com>
2022-02-17 19:43:39 +0330

    add toast component to app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Buttons from "./components/Buttons";
+import Toast from "./components/Toast";
 
 function App() {
   return (
     <div className="App">
       <Buttons />
+      <Toast />
     </div>
   );
 }
